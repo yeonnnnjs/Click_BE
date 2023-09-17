a569a8d3a431027be6b5564dda3fb31208628554
yeonnnnjs <sungyeon52@gmail.com>
2023-09-17 15:48:27 +0900

    Modify for CORS and filter

---
+++ src/main/java/com/yeonnnnjs/click/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.yeonnnnjs.click.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("clickgame.kro.kr")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
+}
